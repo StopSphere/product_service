100deb41b7a4baaefe67f02112765956b3c73892
Monudhakad1 <monudhakad055@gmail.com>
2026-06-06 15:10:55 +0530

    Unit testing done

--- src/test/java/com/shopsphere/product_service/product_service/ServiceTest/ProductServiceImplTest.java
+++ src/test/java/com/shopsphere/product_service/product_service/ServiceTest/ProductServiceImplTest.java
@@ -1,7 +1,10 @@
 package com.shopsphere.product_service.product_service.ServiceTest;
 
+import com.shopsphere.product_service.product_service.DTO.request.CreateProductRequestDTO;
+import com.shopsphere.product_service.product_service.DTO.request.UpdateProductRequestDTO;
+import com.shopsphere.product_service.product_service.DTO.response.PagedResponse;
+import com.shopsphere.product_service.product_service.DTO.response.ProductResponseDTO;
 import com.shopsphere.product_service.product_service.Entity.Product;
-import com.shopsphere.product_service.product_service.Exception.InsufficientStockException;
 import com.shopsphere.product_service.product_service.Exception.ProductNotFoundException;
 import com.shopsphere.product_service.product_service.Mapper.ProductMapper;
 import com.shopsphere.product_service.product_service.Repository.ProductRepository;
@@ -11,73 +14,224 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class ProductServiceImplTest {
+class ProductServiceImplTest {
+
     @Mock
     private ProductRepository productRepository;
-
-    @InjectMocks
-    private ProductServiceImpl productService;
 
     @Mock
     private ProductMapper productMapper;
 
+    @InjectMocks
+    private ProductServiceImpl productService;
+
     @Test
-    void shouldReduceStockSuccessfully() {
-        UUID productId = UUID.randomUUID();
+    void shouldCreateProductSuccessfully() {
+
+        CreateProductRequestDTO request =
+                new CreateProductRequestDTO();
+
         Product product = new Product();
-        product.setId(productId);
-        product.setQuantity(10);
-        product.setAvailable(true);
-        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
-        productService.reduceStock(productId, 5);
-        assertEquals(5, product.getQuantity());
-        assertTrue(product.isAvailable());
-        verify(productRepository).save(product);
+
+        ProductResponseDTO response =
+                new ProductResponseDTO();
+
+        when(productMapper.toEntity(request))
+                .thenReturn(product);
+
+        when(productMapper.toResponseDTO(product))
+                .thenReturn(response);
+
+        ProductResponseDTO result =
+                productService.createProduct(request);
+
+        assertEquals(response, result);
+
+        verify(productRepository)
+                .save(product);
     }
 
     @Test
-    void shouldThrowException() {
+    void shouldUpdateProductSuccessfully() {
+
         UUID productId = UUID.randomUUID();
+
         Product product = new Product();
-        product.setId(productId);
-        product.setQuantity(3);
-        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
 
-        assertThrows(InsufficientStockException.class, () -> productService.reduceStock(productId, 5));
+        UpdateProductRequestDTO request =
+                new UpdateProductRequestDTO();
 
-        verify(productRepository, never()).save(any());
+        ProductResponseDTO response =
+                new ProductResponseDTO();
+
+        when(productRepository.findById(productId))
+                .thenReturn(Optional.of(product));
+
+        when(productMapper.toResponseDTO(product))
+                .thenReturn(response);
+
+        ProductResponseDTO result =
+                productService.updateProduct(
+                        productId,
+                        request
+                );
+
+        assertEquals(response, result);
+
+        verify(productMapper)
+                .updateProductFromDto(
+                        request,
+                        product
+                );
+
+        verify(productRepository)
+                .save(product);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenUpdatingUnknownProduct() {
+
+        UUID productId = UUID.randomUUID();
+
+        when(productRepository.findById(productId))
+                .thenReturn(Optional.empty());
+
+        assertThrows(
+                ProductNotFoundException.class,
+                () -> productService.updateProduct(
+                        productId,
+                        new UpdateProductRequestDTO()
+                )
+        );
+    }
+
+    @Test
+    void shouldGetProductByIdSuccessfully() {
+
+        UUID productId = UUID.randomUUID();
+
+        Product product = new Product();
+
+        ProductResponseDTO response =
+                new ProductResponseDTO();
+
+        when(productRepository.findById(productId))
+                .thenReturn(Optional.of(product));
+
+        when(productMapper.toResponseDTO(product))
+                .thenReturn(response);
+
+        ProductResponseDTO result =
+                productService.getProductById(productId);
+
+        assertEquals(response, result);
+
+        verify(productRepository)
+                .findById(productId);
     }
 
     @Test
     void shouldThrowExceptionWhenProductNotFound() {
+
         UUID productId = UUID.randomUUID();
-        when(productRepository.findById(productId)).thenReturn(Optional.empty());
-        assertThrows(ProductNotFoundException.class, () -> productService.reduceStock(productId, 5));
+
+        when(productRepository.findById(productId))
+                .thenReturn(Optional.empty());
+
+        assertThrows(
+                ProductNotFoundException.class,
+                () -> productService.getProductById(productId)
+        );
     }
 
     @Test
-    void shouldUpdateStockSuccessfully() {
+    void shouldDeleteProductSuccessfully() {
+
         UUID productId = UUID.randomUUID();
-        Product product = new Product();
-        product.setId(productId);
-        product.setQuantity(10);
 
-        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
+        productService.deleteProduct(productId);
 
-        productService.updateStock(productId, 5);
-        assertEquals(5, product.getQuantity());
-        assertTrue(product.isAvailable());
-        verify(productRepository).save(product);
+        verify(productRepository)
+                .deleteById(productId);
     }
 
+    @Test
+    void shouldSearchProductsByNameSuccessfully() {
 
+        Product product = new Product();
+
+        ProductResponseDTO response =
+                new ProductResponseDTO();
+
+        Page<Product> page =
+                new PageImpl<>(List.of(product));
+
+        when(
+                productRepository
+                        .findByNameContainingIgnoreCase(
+                                eq("iphone"),
+                                any(Pageable.class)
+                        )
+        ).thenReturn(page);
+
+        when(productMapper.toResponseDTO(product))
+                .thenReturn(response);
+
+        PagedResponse<ProductResponseDTO> result =
+                productService.searchProductsByName(
+                        "iphone",
+                        0,
+                        10
+                );
+
+        assertEquals(
+                1,
+                result.getProducts().size()
+        );
+    }
+
+    @Test
+    void shouldGetAllProductsSuccessfully() {
+
+        Product product = new Product();
+
+        ProductResponseDTO response =
+                new ProductResponseDTO();
+
+        Page<Product> page =
+                new PageImpl<>(List.of(product));
+
+        when(
+                productRepository.findAll(
+                        any(Pageable.class)
+                )
+        ).thenReturn(page);
+
+        when(productMapper.toResponseDTO(product))
+                .thenReturn(response);
+
+        PagedResponse<ProductResponseDTO> result =
+                productService.getAllProducts(
+                        0,
+                        10,
+                        "name",
+                        "asc"
+                );
+
+        assertEquals(
+                1,
+                result.getProducts().size()
+        );
+    }
 }
-
